1f4f54b0b4ded1dffbec8e01533e8b948466cb85
최우영 <choeuyeong@choeuyeong-ui-MacBookAir.local>
2025-10-26 17:44:08 +0900

    이름입력 및 자동차 객체 생성하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static class Car {
+        private final String name;
+        public Car(String name) {
+            this.name = name;
+        }
+        public String getName() {return this.name;};
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+        String[] names = input.split(",");
+
+        List<Car> carList = new ArrayList<>();
+        for (String name : names) {
+            carList.add(new Car(name.trim()));
+        }
+
+
     }
 }
